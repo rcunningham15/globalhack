549b9399c33aa9cdb2a46f20b061be9080c08c27
Rebecca Cunningham <rebecca.a.cunningham26@gmail.com>
2018-07-24 20:37:58 +0000

    afternoon1

---
+++ magic8.py
@@ -0,0 +1,6 @@
+import random
+while (1 == 1):
+    x = raw_input("Ask me a question:")
+    y = random.choice(["It is certain.","Yes - definitely.","As I see it, yes.","Most likely.","Reply hazy, try again","Better not tell you now.","Don't count on it.","My sources say no."])
+    print(y)
+    print("Play again?")
