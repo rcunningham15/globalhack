03386b73d0bb27566ef7dcaf9fd21056d11c3020
Rebecca Cunningham <rebecca.a.cunningham26@gmail.com>
2018-07-27 16:31:10 +0000

    jkl

---
+++ gofish.py
@@ -0,0 +1,13 @@
+import random
+
+deck = []
+count1 = 0
+count2 = 0
+pdeck = []
+cdeck = []
+
+#create deck
+while (count1 < 4):
+    for x in range(2,15):
+        deck.append(x)
+    count1 = count1 + 1
